d8416bbb734407c5979f80f1e99744607b102a10
Víctor Falcón <victoor89@gmail.com>
2023-11-20 14:28:58 +0100

    Custom Tokyonight style

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -3,6 +3,17 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       transparent = true,
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
     },
   },
 }
